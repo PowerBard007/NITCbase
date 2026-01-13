056ddadd758cf2dd3ce1ac69474741711f824cf0
Aakansh <aakansh.anil04@gmail.com>
2026-01-13 14:50:27 +0000

    stage3 working

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,83 @@
 
 #include <cstdlib>
 #include <cstring>
+
+// the declarations for these functions can be found in "BlockBuffer.h"
+
+BlockBuffer:: BlockBuffer(int blockNum){
+    //initialise thi.blockNum with the argument
+    this->blockNum=blockNum;
+}
+
+//calls the [arent class constructor
+RecBuffer:: RecBuffer(int blockNum) : BlockBuffer(blockNum){}
+/*
+Used to load a block to the buffer and get a pointer to it.
+NOTE: this function expects the caller to allocate memory for the argument
+*/
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **bufferPtr){
+    // check whether the bloack is already present in the buffer using StaticBuffer.getBufferNum()
+    int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+    if(bufferNum == E_BLOCKNOTINBUFFER || bufferNum == E_OUTOFBOUND){
+        bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+        if(bufferNum == E_OUTOFBOUND){
+            return E_OUTOFBOUND;
+        }
+
+        Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+    }
+
+    // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+    *bufferPtr = StaticBuffer::blocks[bufferNum];
+
+    return SUCCESS;
+}
+//load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head){
+    unsigned char *bufferPtr;
+
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+
+    //populate the numEntries, numAttrs and numSlots fields in*head
+    memcpy(&head->numSlots,bufferPtr+24,4);
+    memcpy(&head->numEntries,bufferPtr+16,4);
+    memcpy(&head->numAttrs,bufferPtr+20,4);
+    memcpy(&head->rblock,bufferPtr+12,4);
+    memcpy(&head->lblock,bufferPtr+8,4);
+
+    return SUCCESS;
+}
+
+//load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec,int slotNum){
+    struct HeadInfo head;
+    this->getHeader(&head);
+    // get the header using this.getHeader() function
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char *bufferPtr;
+
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if(ret != SUCCESS){
+        return ret;
+    }
+    /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+        - each record will have size attrCount * ATTR_SIZE
+        - slotMap will be of sisze slotCount
+    */
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+    int offset = HEADER_SIZE + slotCount + (recordSize * slotNum);
+
+    // unsigned char *slotPointer = &buffer/* calculate buffer + offset */;
+
+    // load the record into the rec data structure
+    memcpy(rec, bufferPtr + offset, recordSize);
+
+    return SUCCESS;
+}
